7fa0f9d4ceddfc508a11b1cbe3454e4780917a03
Vinicius66966 <witorcarvalho00@gmail.com>
2024-12-13 18:08:04 -0300

    produção-segunda-seção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,31 @@ export default function Home() {
           <p className={styles.subtitle}>O LAR PERFEITO</p>
           <h1 className={styles.mainText}>existe</h1>
           <div className={styles.logoContainer}>
-            <img
-              src="/images/logo.png"
-              alt="Logo"
-              className={styles.logo}
-            />
+           
           </div>
         </div>
       </div>
-      <div className={styles.secondaryPage}> {/* Segunda seção com background */}
-        <p className={styles.subtitle}>Segunda Seção</p>
-        <h1 className={styles.mainText}>Outro Conteúdo</h1>
-        <div className={styles.logoContainer}>
-          <img
-            src="/images/logo.png"
-            alt="Logo"
-            className={styles.logo}
-          />
-        </div>
-      </div>
+
+  <div className={styles.secondaryPage}>
+  <div className={styles.contentWrapper}>
+  <h1 className={styles.paragrafo1}> SEU LAR DE</h1>
+  <h2 className={styles.paragrafo2}> ALTO PADRÃO</h2>
+    <div className={styles.imageContainer}>
+      <img
+        src="/images/familia.png"
+        alt="Familia"
+        className={styles.familiaImage}
+      />
+    </div>
+    <div className={styles.videoContainer}>
+      <video width="100%" height="auto" controls>
+        <source src="/videos/sample-video.mp4" type="video/mp4" />
+       
+      </video>
+    </div>
+  </div>
+</div>
+
       <div className={styles.tertiaryPage}> {/* Terceira seção com novo background */}
         <p className={styles.subtitle}>Terceira Seção</p>
         <h1 className={styles.mainText}>Mais Conteúdo</h1>
@@ -39,6 +45,7 @@ export default function Home() {
           />
         </div>
       </div>
+
       <div className={styles.quaternaryPage}> {/* Quarta seção com novo background */}
         <p className={styles.subtitle}>Quarta Seção</p>
         <h1 className={styles.mainText}>Último Conteúdo</h1>
@@ -50,6 +57,7 @@ export default function Home() {
           />
         </div>
       </div>
+
       <div className={styles.quinaryPage}> {/* Quinta seção com novo background */}
         <p className={styles.subtitle}>Quinta Seção</p>
         <h1 className={styles.mainText}>Conteúdo Adicional</h1>
